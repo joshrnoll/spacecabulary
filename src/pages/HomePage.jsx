706dbd9c4120e967e53c298d6bf41e0e78b72b3b
Kevin <kevin.velazquez.pz@gmail.com>
2025-08-07 17:56:19 -0400

    End of Day 2 commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import "../styles/App.css";
 import { useNavigate } from "react-router-dom";
+import UserDeck from '../components/UserDeck';
 
 export default function HomePage() {
   const navigate = useNavigate();
 
   return (
     <>
+      <UserDeck></UserDeck>
+      <br/>
       <h1>You have no cards!</h1>
       <button id="add-cards" onClick={() => navigate("/add-card-page")}>
         Add Cards
