56f98743970bcb76b2208a8c66b6f407a3868ba8
OverKoffee <redmaldeveloper@gmail.com>
2025-08-07 14:43:04 -0700

    refactored

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,15 @@
 import "../styles/App.css";
 import { useNavigate } from "react-router-dom";
-import { useState, useContext, createContext } from "react";
 
-const DeckContext = createContext();
-export const useDeckContext = () => useContext(DeckContext);
-
-function HomePage() {
+export default function HomePage() {
   const navigate = useNavigate();
-  const [reviewDeck, setReviewDeck] = useState(["yep", "more yep"]);
 
   return (
     <>
-      <DeckContext.Provider
-        value={{
-          reviewDeck,
-        }}
-      >
-        <h1>You have no cards!</h1>
-        <button id="add-cards" onClick={() => navigate("/add-card-page")}>
-          Add Cards
-        </button>
-      </DeckContext.Provider>
+      <h1>You have no cards!</h1>
+      <button id="add-cards" onClick={() => navigate("/add-card-page")}>
+        Add Cards
+      </button>
     </>
   );
-  );
 }
-
-export default HomePage;
-export default HomePage;
