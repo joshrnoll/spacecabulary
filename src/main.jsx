4a1e979c3692502b653e74a03462d517a01d1ff8
Casey Houle <c.j.houle75@gmail.com>
2025-08-08 09:47:37 -0700

    cleaned up unnecessary files

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-createRoot(document.getElementById('root')).render(
-    <Router>
-        <App />
-    </Router>
-)
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
+);
